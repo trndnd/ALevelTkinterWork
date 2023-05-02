6db9b5c6a81f0a6aa1333be4d9f501784133468e
trndnd <65019452+trndnd@users.noreply.github.com>
2023-05-02 11:52:56 +0100

    Update TutorialGui.py
    Change the GUI looks a bit and attempted to make a function where it gets the password of the username and compares it to the password inputted. If it does then it says a message. There is no function which expands on this as it's just a log in system. The function doing this is GettingPasswordFromDatabaseAndChecking().
    Also made it so that the window is destroyed after the login or register button is pressed. 
    Things to do:
    Make the GettingPasswordFromDatabaseAndChecking work properly as it's not fetching the correct data
    Maybe use recursission to force the user to input valid data. So if it's not valid or incorrect then current window is destroyed and the function registerpage() is called again.
    Change the code so that only window is visible at one time by destroying a previous one before a new one can be made. Maybe even implement q button at the top left to go back.

--- TkinterForschool/MakingGUi/TutorialGui.py
+++ TkinterForschool/MakingGUi/TutorialGui.py
@@ -1,14 +1,15 @@
 from tkinter import *
 from tkinter import messagebox
+import time
 import sqlite3
 MainScreen = Tk()
-MainScreen.geometry("300x2")
+MainScreen.geometry("300x250")
 db = sqlite3.connect("CustomerLogIn.db")
 cursor = db.cursor()
 def MainPage():
-    TitleLabel = Label(MainScreen, text="Main Page").pack()
-    RegisterButton = Button(MainScreen, text="Register", command= RegisterPage).pack()
-    LoginButton = Button(MainScreen,text="Log in", command= LoginPage).pack()
+    TitleLabel = Label(MainScreen, text="Main Page").pack(pady = 10)
+    RegisterButton = Button(MainScreen, text="Register", command= RegisterPage).pack(pady = 10)
+    LoginButton = Button(MainScreen,text="Log in", command= LoginPage).pack(pady = 10)
     
 
 def LoginPage():
@@ -16,24 +17,49 @@ def LoginPage():
     UsernameInputted = StringVar()
     PasswordInputted = StringVar()
     LoginScreen = Toplevel(MainScreen) # Makes this teh top level screen whihc essentially makes it the new screen from teh main screen
-    UsernameLabel = Label(LoginScreen, text="Username").pack()
-    Entry()
+    LoginScreen.title("Login Window")
+    BigGap = Label(LoginScreen, text ="   ").pack(pady = 100)
+    UsernameLabel = Label(LoginScreen, text="Username").pack(pady = 10)
+    Entry(LoginScreen,textvariable = UsernameInputted).pack(pady = 10)
+    PasswordLabel = Label(LoginScreen, text = "Password").pack()
+    Entry(LoginScreen,textvariable = PasswordInputted, show = "*").pack(pady = 10)
+    LoginButton = Button(LoginScreen, text ="Log in",command=lambda: GettingPasswordFromDatabaseAndChecking(UsernameInputted.get(),PasswordInputted.get(),LoginScreen)).pack()
     
 def RegisterPage():
     global MainScreen
     UsernameInputted = StringVar()
     PasswordInputted = StringVar()
     RegisterScreen = Toplevel(MainScreen) # Makes this teh top level screen whihc essentially makes it the new screen from teh main screen
-    UsernameLabel = Label(RegisterScreen, text="Username").pack()
-    Entry(RegisterScreen, textvariable= UsernameInputted).pack()
-    Entry(RegisterScreen,textvariable= PasswordInputted, show="*").pack()
-    RegisterButton = Button(RegisterScreen, text = "register", command=lambda: CheckingDataIsValid(UsernameInputted.get(),PasswordInputted.get())).pack()
+    RegisterScreen.title("Register Window")
+    BigGap = Label(RegisterScreen, text ="   ").pack(pady = 100)
+    UsernameLabel = Label(RegisterScreen, text="Username").pack(pady = 10)
+    Entry(RegisterScreen, textvariable= UsernameInputted).pack(pady = 10)
+    PasswordLabel = Label(RegisterScreen, text = "Password").pack()
+    Entry(RegisterScreen,textvariable= PasswordInputted, show="*").pack(pady = 10)
+    RegisterButton = Button(RegisterScreen, text = "register", command=lambda: CheckingDataIsValid(UsernameInputted.get(),PasswordInputted.get())).pack(pady = 10)
+    
 
 def AddingToDatabase(UsernameInputted,Passwordinputted):
     cursor.execute(f"INSERT INTO Customers Values(\"{UsernameInputted}\",\"{Passwordinputted}\")") # Need apostrpahes as it makes them strings for the db
     db.commit()
-    
+
+def GettingPasswordFromDatabaseAndChecking(UsernameInputted,PasswordInputted,LoginScreen):
+    print(UsernameInputted)
+    cursor.execute("SELECT Password FROM Customers WHERE Username = \"{UsernameInputted}\"")
+    PasswordOfAccount = cursor.fetchall()
+    print(PasswordOfAccount)
+    if PasswordOfAccount == PasswordInputted:
+        messagebox.showinfo("showinfo","Successfully logged in")
+        #There should be a function here to take you to the next page but teher is no page so it just shows that you have logged in!
+    elif PasswordOfAccount == None:
+        messagebox.showinfo("showinfo","Username does not exist. Go register!")
+        RegisterPage()
+    else:
+        messagebox.showinfo("showinfo","Incorrect password or username")
+    LoginScreen.destroy()
+        
 def CheckingDataIsValid(UsernameInputted,PasswordInputted):
+    global RegisterScreen
     cursor.execute("SELECT Username FROM Customers")
     AllUsernames = [row[0] for row in cursor.fetchall()] # got rid of the comments as the problem with comparing the two was that the username form teh database had an extra comment at the end
     print(UsernameInputted)
@@ -44,9 +70,10 @@ def CheckingDataIsValid(UsernameInputted,PasswordInputted):
         messagebox.showinfo("showinfo", "Password should be over 10 characters")
     else:
         AddingToDatabase(UsernameInputted,PasswordInputted)
+        messagebox.showinfo("showinfo",f"You have successfully registered with the username {UsernameInputted}")
+        TestLabel = Label(RegisterScreen, text="You have successfully registerd").pack()
+        time.sleep(3)
+    RegisterScreen.destroy()
         
-a = "aad"
-mylist = [("aaron"),("aad")]
-print(a in mylist)
 MainPage()
 MainScreen.mainloop()
